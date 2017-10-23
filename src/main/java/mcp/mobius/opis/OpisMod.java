c1a3ec533c2c68bc0c75ef86ba9966df06352936
phit <phit@hush.com>
2017-10-23 22:21:17 +0200

    bump version

--- src/main/java/mcp/mobius/opis/OpisMod.java
+++ src/main/java/mcp/mobius/opis/OpisMod.java
@@ -28,7 +28,7 @@ public class OpisMod {
 
     public static final String NAME = "OpisReloaded";
     public static final String MODID = "opisreloaded";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.3";
 
     @Mod.Instance(OpisMod.MODID)
     public static OpisMod instance;
